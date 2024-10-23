e137f6899df5835771bde7c587236191002aa1a8
mcdonalddylan <soupaface@gmail.com>
2024-10-23 00:25:27 -0500

    Add simple footer + Styling update on homepage + Mobile fix
    There were so many changes included in this commit, almost all of them were meant to clean up the look of the homepage.

--- assets/application.js
+++ assets/application.js
@@ -3,22 +3,29 @@
         const location = window.location.pathname;
         const navigationLinksList = document.querySelectorAll('.cmp-nav__container a');
     
-        console.log(location);
-        console.log(navigationLinksList);
-    
         const spinningImg = document.createElement('div');
         spinningImg.classList.add('cmp-nav__spinner');
+
+        /**
+         * Add Spinner To Nav Element
+         * @param {HTMLElement} navElement 
+         * @param {HTMLElement} spinnerElement 
+         */
+        const addSpinnerToNavElement = (navElement, spinnerElement) => {
+            navElement.classList.add('cmp-nav__nav-link--selected');
+            navElement.append(spinnerElement);
+        } 
     
         if (navigationLinksList.length > 0 && location === '/') {
-            navigationLinksList[0].append(spinningImg);
+            addSpinnerToNavElement(navigationLinksList[0], spinningImg);
         } else if (navigationLinksList.length > 0 && location === '/collections/all') {
-            navigationLinksList[1].append(spinningImg);
+            addSpinnerToNavElement(navigationLinksList[1], spinningImg);
         } else if (navigationLinksList.length > 0 && location === '/pages/contact') {
-            navigationLinksList[2].append(spinningImg);
+            addSpinnerToNavElement(navigationLinksList[2], spinningImg);
         } else if (navigationLinksList.length > 0 && location === '/cart') {
-            navigationLinksList[3].append(spinningImg);
+            addSpinnerToNavElement(navigationLinksList[3], spinningImg);
         } else if (navigationLinksList.length > 0 && location === '/account') {
-            navigationLinksList[4].append(spinningImg);
+            addSpinnerToNavElement(navigationLinksList[4], spinningImg);
         }
     });
 })();
