9286f676a67db4a44797d89faca026788eab6443
mcdonalddylan <soupaface@gmail.com>
2024-11-06 21:52:28 -0600

    Added search functionality + Fixed numerous stlying issues + Updated Readme

--- README.md
+++ README.md
@@ -1,6 +1,12 @@
 # Test-Theme-1
-## Dylan McD
+## Dylan McDonald
 
-![Theme progress as of 11/4/24](assets/shopify-theme-1-progress2.gif "Theme progress")
+### https://dylanmctest.myshopify.com
+
+#### Desktop & Tablet view
+![Theme progress as of 11/6/24](assets/shopify-theme-1-progress3.gif "Desktop and tablet theme progress")
+
+#### Mobile view
+![Theme progress as of 11/6/24](assets/shopify-theme-1-progress3-mobile.gif "Mobile theme progress")
 
 First attempt at creating a shopify liquid theme
--- assets/application.js
+++ assets/application.js
@@ -44,8 +44,7 @@
          * Toggle Show Dropdown
          * @param {*} event 
          */
-        const toggleShowDropdown = (event) => {
-            event.preventDefault();
+        const toggleShowDropdown = () => {
             if (accountDropdown.classList.contains('cmp-account-dropdown--hide')) {
                 accountDropdown.classList.remove('cmp-account-dropdown--hide');
             } else {
@@ -54,6 +53,102 @@
         }
 
         accountButton.addEventListener('click', toggleShowDropdown);
-        accountButton.addEventListener('blur', toggleShowDropdown);
+        accountButton.addEventListener('blur', () => {
+            // Need to use a timeout to actually detect the document.activeElement for some reason :,)
+            setTimeout(() => {
+                if (!document.activeElement.id.includes('customer_login_link') &&
+                    !document.activeElement.id.includes('customer_register_link') &&
+                    !document.activeElement.id.includes('customer_logoff_link')) {
+                    toggleShowDropdown();
+                }
+            }, 0);
+        });
+
+        // ++++++++++++++++
+        // Search Results Accordion
+        // ++++++++++++++++
+
+        const searchResultButtons = document.querySelectorAll('.cmp-search-page__result');
+
+        /**
+         * Toggle Show Product Description
+         * @param {*} event 
+         */
+        const toggleShowProductDesc = (event) => {
+            const searchResultsButton = event.currentTarget;
+            if (searchResultsButton.classList.contains('cmp-search-page__result--tray-hidden')) {
+                searchResultsButton.classList.remove('cmp-search-page__result--tray-hidden');
+            } else {
+                searchResultsButton.classList.add('cmp-search-page__result--tray-hidden');
+            }
+        }
+
+        if (searchResultButtons.length > 0) {
+            for (const searchResultButton of searchResultButtons) {
+                searchResultButton.addEventListener('click', (event) => {
+                    if (event.target.classList.contains('cmp-search-page__result-row') ||
+                        event.target.classList.contains('cmp-search-page__result') ||
+                        event.target.classList.contains('cmp-search-page__result-chev')) {
+                        toggleShowProductDesc(event);
+                    }
+                });
+            }
+        }
+
+        // ++++++++++++++++
+        // Displaying Reset Button on Search Bar
+        // ++++++++++++++++
+
+        const searchBar = document.querySelector('.cmp-search-bar__input');
+        const searchBarResetButton = document.querySelector('.cmp-search-bar__reset-btn');
+
+        /**
+         * Determine If Reset Button Should Display
+         * @param {*} event 
+         */
+        const determineIfResetButtonShouldDisplay = () => {
+            const searchValue = searchBar.value;
+            if (searchValue.length > 0) {
+                searchBarResetButton.classList.remove('cmp-search-bar__reset-btn--hidden');
+            } else {
+                searchBarResetButton.classList.add('cmp-search-bar__reset-btn--hidden');
+            }
+        }
+
+        /**
+         * Reset Search Value
+         * @param {*} event 
+         */
+        const resetSearchValue = (event) => {
+            event.preventDefault();
+            searchBar.value = '';
+            searchBarResetButton.classList.add('cmp-search-bar__reset-btn--hidden');
+        }
+
+        searchBar.addEventListener('change', determineIfResetButtonShouldDisplay);
+        searchBarResetButton.addEventListener('click', resetSearchValue);
+
+        determineIfResetButtonShouldDisplay();
+
+        // ++++++++++++++++
+        // Results-Per-Page Select Logic
+        // ++++++++++++++++
+
+        const resultsPerPageDropdown = document.querySelector('select[name="results-per-page"]');
+
+        /**
+         * Update results per page
+         * @param {*} event 
+         */
+        const updateResultsPerPage = (event) => {
+            event.preventDefault();
+            const resultsPerPage = event.currentTarget.options[event.currentTarget.selectedIndex].value;
+            console.log(resultsPerPage);
+            var url = new URL(window.location.href);
+            url.searchParams.set('limit', resultsPerPage);
+            window.location.href = url.toString();
+        }
+
+        //resultsPerPageDropdown.addEventListener('change', updateResultsPerPage);
     });
 })();
